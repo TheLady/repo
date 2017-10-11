1fc822dd825d1a55cbf42b235a1a966fcec1404d
thelady <dihaynes77@gmail.com>
2017-10-10 21:22:38 -0700

    first coommit

---
+++ main_script.py
@@ -0,0 +1,10 @@
+import config
+
+from twython import Twython, TwythonError
+
+
+
+# create a Twython object by passing the necessary secret passwords
+auth_obj = Twython(config.api_key, config.api_secret, config.access_token, config.token_secret)
+
+
